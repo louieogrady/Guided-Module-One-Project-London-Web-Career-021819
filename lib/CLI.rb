9700ebae8f6f8d9acc31914b6d5ad97b74149992
louieogrady <l.ogrady@gmail.com>
2019-03-07 07:41:30 +0000

    added users - need to add more db methods

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -2,9 +2,8 @@ class CommandLineInterface
 
   def find_or_create_by(username)
     # Find or create new user
-    User.find_or_create_by(username: username.chomp.strip)
+    @u = User.find_or_create_by(username: username.chomp.strip)
   end
-
 
   def run
     system "clear"
@@ -13,15 +12,26 @@ class CommandLineInterface
     menu
   end
 
+  def run2
+    system "clear"
+    return_to_main_menu_greeting
+    initial_options
+    menu
+  end
+
   def greet
     puts "Welcome to Your Music Database, the command line solution to facilitate your music collecting addiction!"
     puts "\nPlease Login."
-    login = gets.chomp
-    find_or_create_by(login)
+    username = gets.chomp
+    find_or_create_by(username)
+  end
+
+  def return_to_main_menu_greeting
+    puts "Welcome to Your Music Database, the command line solution to facilitate your music collecting addiction!"
   end
 
   def initial_options
-    puts "\nPress 1 to search the Discogs API.\nPress 2 to search and edit your own music collection"
+    puts "\nPress 1 to search the Discogs API.\nPress 2 to search and edit your own music collection.\nPress 3 to logout"
   end
 
   def menu
@@ -30,47 +40,54 @@ class CommandLineInterface
     when "1"
       sleep 1
       system "clear"
-      search_discogs_menu
+      search_discogs_api_with_test_account
     when "2"
       sleep 1
       system "clear"
       search_local_db_submenu
+    when "3"
+      sleep 1
+      system "clear"
+      run
     else
       "not a valid entry"
       sleep 1
-      run
+      run2
     end
   end
 
-  def search_discogs_menu
-    puts "\nDo you have a Discogs account? input y for yes, n for no, or any other key for main menu"
-    answer = gets.chomp
-    if answer == "y"
-      puts "\nPlease input your auth key"
-      authkey = gets.chomp
-      auth_wrapper = Discogs::Wrapper.new("My test app", user_token: authkey)
-      sleep 1
-      system "clear"
-      search_discogs_api
-    elsif answer == "n"
-      puts "\nUsing a test account instead"
-      sleep 1
-      system "clear"
-      search_discogs_api_with_test_account
-    else
-      run
-    end
-  end
+  # def search_discogs_menu
+  #   puts "\nDo you have a Discogs account? input y for yes, n for no, or any other key for main menu"
+  #   answer = gets.chomp
+  #   if answer == "y"
+  #     puts "\nPlease input your auth key"
+  #     authkey = gets.chomp
+  #     auth_wrapper = Discogs::Wrapper.new("My test app", user_token: authkey)
+  #     sleep 1
+  #     system "clear"
+  #     search_discogs_api
+  #   elsif answer == "n"
+  #     puts "\nUsing a test account instead"
+  #     sleep 1
+  #     system "clear"
+  #     search_discogs_api_with_test_account
+  #   else
+  #     run2
+  #   end
+  # end
 
   def search_discogs_api_with_test_account
     puts "\nPress 1 to search the Discogs API by release title. Press 2 to return to the main menu."
     answer = gets.chomp
     system "clear"
     if answer == "1"
+      sleep 1
       discogs_release_title_search
     elsif answer == "2"
-      run
+      sleep 1
+      run2
     else
+      sleep 1
       search_discogs_api_with_test_account
     end
   end
@@ -92,10 +109,10 @@ class CommandLineInterface
       puts "Please enter the name of the release you wish to view."
       release_answer = gets.chomp.strip.titleize
       #artist_releases = auth_wrapper.get_artist_releases(artist_search.results.first.id)
-      titles = auth_wrapper.get_artist_releases(artist_search.results.first.id, :per_page =>400).releases.map {|release_titles| release_titles.title}
+      titles = auth_wrapper.get_artist_releases(artist_search.results.first.id, :per_page =>600).releases.map {|release_titles| release_titles.title}
       if titles.find {|titles| titles == release_answer} # checks if the user query matches the release title for that artist.
         puts "Release Found"
-        release_information = auth_wrapper.get_artist_releases(artist_search.results.first.id, :per_page =>400 ).releases.find {|titles| titles.title == release_answer}
+        release_information = auth_wrapper.get_artist_releases(artist_search.results.first.id, :per_page =>600 ).releases.find {|titles| titles.title == release_answer}
         #puts "#{auth_wrapper.get_artist_releases(artist_search.results.first.id).releases.find {|titles| titles.title == release_answer}}" # returns release information
         sleep 1
         puts "\nArtist: #{auth_wrapper.get_artist(artist_search.results.first.id).name}\nTitle:  #{release_information.title}\nReleased: #{release_information.year}\nGenre: #{release_information.genre}\nFormat: #{release_information.format}"
@@ -103,17 +120,18 @@ class CommandLineInterface
         puts "Would you like to add this to your collection? y or n?"
         collection_response = gets.chomp.strip
         if collection_response == "y"
-          Release.create(artist: auth_wrapper.get_artist(artist_search.results.first.id).name, title: release_information.title, released: release_information.year, genre: release_information.genre, format: release_information.format)
+          rel = Release.create(artist: auth_wrapper.get_artist(artist_search.results.first.id).name, title: release_information.title, released: release_information.year, genre: release_information.genre, format: release_information.format)
+          @u.releases << rel
           puts "Added to your music collection. Returning to main menu"
-          run
+          run2
         else
-          search_discogs_menu
+          search_discogs_api_with_test_account
         end
       else
         puts "Release not found"
         sleep 1
         system "clear"
-        search_discogs_menu
+        search_discogs_api_with_test_account
       end
 
     elsif response == "n"
@@ -127,10 +145,10 @@ class CommandLineInterface
         puts "\nPlease enter the name of the release you wish to view."
         release_answer = gets.chomp.strip.titleize
         #artist_releases = auth_wrapper.get_artist_releases(artist_search.results.first.id)
-        titles = auth_wrapper.get_artist_releases(artist_search.results.second.id, :per_page =>400).releases.map {|release_titles| release_titles.title}
+        titles = auth_wrapper.get_artist_releases(artist_search.results.second.id, :per_page =>600).releases.map {|release_titles| release_titles.title}
         if titles.find {|titles| titles == release_answer} # checks if the user query matches the release title for that artist.
           puts "Release Found"
-          release_information = auth_wrapper.get_artist_releases(artist_search.results.second.id, :per_page =>400).releases.find {|titles| titles.title == release_answer}
+          release_information = auth_wrapper.get_artist_releases(artist_search.results.second.id, :per_page =>600).releases.find {|titles| titles.title == release_answer}
           #puts "#{auth_wrapper.get_artist_releases(artist_search.results.first.id).releases.find {|titles| titles.title == release_answer}}" # returns release information
           sleep 1
           puts "\nArtist: #{auth_wrapper.get_artist(artist_search.results.second.id).name}\nTitle:  #{release_information.title}\nReleased: #{release_information.year}\nGenre: #{release_information.genre}\nFormat: #{release_information.format}"
@@ -140,15 +158,15 @@ class CommandLineInterface
           if collection_response == "y" || "yes" || "Yes"
             Release.create(artist: auth_wrapper.get_artist(artist_search.results.second.id).name, title: release_information.title, released: release_information.year, genre: release_information.genre, format: release_information.format)
             puts "Added to your music collection. Returning to main menu"
-            run
+            run2
           else
-            search_discogs_menu
+            search_discogs_api_with_test_account
           end
         else
           puts "Release not found"
           sleep 1
           system "clear"
-          search_discogs_menu
+          search_discogs_api_with_test_account
         end
 
     elsif response2 == "n"
@@ -161,10 +179,10 @@ class CommandLineInterface
             puts "\nPlease enter the name of the release you wish to view."
             release_answer = gets.chomp.titleize
             #artist_releases = auth_wrapper.get_artist_releases(artist_search.results.first.id)
-            titles = auth_wrapper.get_artist_releases(artist_search.results.third.id, :per_page =>400).releases.map {|release_titles| release_titles.title}
+            titles = auth_wrapper.get_artist_releases(artist_search.results.third.id, :per_page =>600).releases.map {|release_titles| release_titles.title}
             if titles.find {|titles| titles == release_answer} # checks if the user query matches the release title for that artist.
               puts "Release Found"
-              release_information = auth_wrapper.get_artist_releases(artist_search.results.third.id, :per_page =>400).releases.find {|titles| titles.title == release_answer}
+              release_information = auth_wrapper.get_artist_releases(artist_search.results.third.id, :per_page =>600).releases.find {|titles| titles.title == release_answer}
               #puts "#{auth_wrapper.get_artist_releases(artist_search.results.first.id).releases.find {|titles| titles.title == release_answer}}" # returns release information
               sleep 1
               puts "\nArtist: #{auth_wrapper.get_artist(artist_search.results.third.id).name}\nTitle:  #{release_information.title}\nReleased: #{release_information.year}\nGenre: #{release_information.genre}\nFormat: #{release_information.format}"
@@ -174,16 +192,16 @@ class CommandLineInterface
               if collection_response == "y" || "yes" || "Yes"
                 Release.create(artist: auth_wrapper.get_artist(artist_search.results.third.id).name, title: release_information.title, released: release_information.year, genre: release_information.genre, format: release_information.format)
                 puts "Added to your music collection. Returning to main menu"
-                run
+                run2
               else
                 system "clear"
-                search_discogs_menu
+                search_discogs_api_with_test_account
               end
             else
               puts "Release not found"
               sleep 1
               system "clear"
-              search_discogs_menu
+              search_discogs_api_with_test_account
             end
 
         elsif response3 == "n"
@@ -220,7 +238,7 @@ class CommandLineInterface
     elsif answer == "m"
       sleep 1
       system "clear"
-      run
+      run2
     else
       sleep 1
       system "clear"
@@ -250,15 +268,19 @@ class CommandLineInterface
     elsif answer == "n"
       sleep 1
       system "clear"
-      run
+      run2
     else
       sleep 1
       search_again?
     end
   end
 
+  def sorted_releases_by_artist_name
+    @u.releases.sort_by { |release| release.artist }
+  end
+
   def return_all_artists_and_releases
-    Release.all.each do |releases|
+    sorted_releases_by_artist_name.each do |releases|
       puts "artist: #{releases.artist} title: #{releases.title} released: #{releases.released} genre: #{releases.genre} format: #{releases.format}"
     end
     puts "\npress m to return to the music collection search menu\n"
@@ -273,9 +295,10 @@ class CommandLineInterface
   end
 
   def search_local_db_by_release_title
-    puts "Please enter a release title to search the database"
+    puts "Please enter a release title to search your database"
     title = gets.chomp.titleize
-    if r = Release.find_by_title(title)
+    #if r = Release.find_by_title(title)
+    if r = @u.releases.find_by_title(title)
       puts "\n artist: #{r.artist}, title: #{r.title}, released: #{r.released}, genre: #{r.genre}, format: #{r.format}"
       search_again?
     else
@@ -303,16 +326,12 @@ class CommandLineInterface
       elsif answer == "m"
         sleep 1
         system "clear"
-        run
+        run2
       else
         nil
       end
     end
   end
-
-
-
-
 
 end
 
